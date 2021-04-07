edf473e4972aeefa463de2056cf50209942cb0c5
Eror Aleksandar <eror.aleksandar01@gmail.com>
2021-04-08 00:11:51 +0200

    First Commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const form = document.querySelector('.form');
+const input = document.querySelector('.input');
+const todoList = document.querySelector('.todo-list');
+
+function createTodo(){
+    let todoValue = input.value;
+
+    // Creating Li
+    const todo = document.createElement('li');
+    todo.classList.add('todo');
+    todo.innerText = todoValue;
+    todoList.appendChild(todo);
+    // Creating button for trash
+    const trashBtn = document.createElement('button');
+    trashBtn.classList.add('trash-btn');
+    trashBtn.innerHTML = `
+        <span class="fas fa-trash"></span>
+    `;
+    todo.appendChild(trashBtn);
+    input.value = '';
+    updateLS();
+
+    todo.addEventListener('click', () => {
+        todo.classList.toggle('done');
+        updateLS();
+    });
+    trashBtn.addEventListener('click', () => {
+        todo.classList.add('rotate');
+        setTimeout(() => {
+            todo.remove();
+        }, 500)
+        updateLS();
+    });
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    createTodo();
+});
